d73cf4fa9c214f66ebda4ed24283ff35408339eb
Roman Bleyer <rbleyer@student.saintaug.nsw.edu.au>
2025-06-21 23:43:35 +1000

    created framework for project in main

---
+++ main.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+
+
+class PasswordStrengthChecker:
+    def __init__(self, master):
+        self.master = master
+
+        self.master.resizable(False, False) # prevent resizing
+        self.master.geometry(f"500x200+{self.master.winfo_screenwidth()-510}+{self.master.winfo_screenheight()-280}") # force window size and position when opened
+
+        # ------------------------------------------------------------------ #
+
+        self.what_page_am_i_on = "main_menu"
+
+        if self.what_page_am_i_on == "main_menu":
+            # all displays for main menu here
+            pass
+        elif self.what_page_am_i_on == "password_generator_settings_menu":
+            # all displays for password generator settings menu here
+            pass
+        elif self.what_page_am_i_on == "about_me_and_help_menu":
+            # all displays for about me and help menu here
+            pass
+            
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PasswordStrengthChecker(root)
+    root.mainloop()
